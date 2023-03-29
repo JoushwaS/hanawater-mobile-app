22c958092a4765ceeef9a6201e633ebd3905c68c
Joushwa <jshahzad@sunbonn.com>
2023-03-29 11:04:59 +0500

    new update available feature push

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "react-native-gesture-handler";
-import React, { useEffect, useState } from "react";
-import { ActivityIndicator, StatusBar, Linking } from "react-native";
+import React, { useEffect, useState, useRef } from "react";
+import {
+  ActivityIndicator,
+  StatusBar,
+  Linking,
+  View,
+  Text,
+} from "react-native";
 import MainNavigation from "./navigation";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
@@ -16,22 +22,44 @@ import { useTranslation } from "react-i18next";
 import VersionCheck from "react-native-version-check";
 import VersionNumber from "react-native-version-number";
 import { Platform } from "react-native";
-
+import { Modal, CustomButton } from "./components";
+import metrix from "./config/metrix";
 function App() {
   const [isNetworkConnected, setisNetworkConnected] = useState(null);
+  const [showUpdateAvailable, setshowUpdateAvailable] = useState(false);
+  const [bundleIdentifier, setbundleIdentifier] = useState("");
   const { t } = useTranslation();
+  const showUpdateModalRef = useRef(null);
 
+  const handleRedirectToStore = () => {
+    if (Platform.OS === "android") {
+      const appUrl = `market://details?id=${bundleIdentifier}`;
+
+      Linking.openURL(appUrl).catch((error) => {
+        console.log("Error opening app URL:", error);
+      });
+      setshowUpdateAvailable(false);
+    } else {
+      const appUrl = `https://apps.apple.com/app/${bundleIdentifier}`;
+      Linking.openURL(appUrl).catch((error) => {
+        console.log("Error opening app URL:", error);
+      });
+      setshowUpdateAvailable(false);
+    }
+  };
   useEffect(() => {
     VersionCheck.getLatestVersion({
-      packageName:
-        Platform.OS === "android" ? "com.hanawater" : "com.apps.hanawater", // Replace with your app's package name
+      packageName: VersionNumber.bundleIdentifier, // Replace with your app's package name
     })
       .then((latestVersion) => {
         console.log("Latest version:>>", latestVersion);
         console.log(VersionNumber.appVersion);
-
+        if (VersionNumber.appVersion != latestVersion) {
+          setshowUpdateAvailable(true);
+        }
         console.log(VersionNumber.buildVersion);
         console.log(VersionNumber.bundleIdentifier);
+        setbundleIdentifier(VersionNumber.bundleIdentifier);
       })
       .catch((error) => {
         console.log("Error checking for latest version:", error);
@@ -85,6 +113,80 @@ function App() {
             translucent
             backgroundColor={Colors.White}
           />
+          <Modal
+            viewRef={showUpdateModalRef}
+            setModalVisible={setshowUpdateAvailable}
+            modalVisible={showUpdateAvailable}
+          >
+            <View
+              style={{
+                width: metrix.HorizontalSize(370),
+                height: metrix.VerticalSize(300),
+                backgroundColor: Colors.White,
+                borderRadius: metrix.VerticalSize(10),
+                paddingVertical: metrix.VerticalSize(25),
+                paddingHorizontal: metrix.HorizontalSize(35),
+                shadowColor: "#000",
+                shadowOpacity: 0.5,
+                shadowRadius: 15,
+                justifyContent: "center",
+
+                elevation: 5,
+              }}
+            >
+              <View>
+                <Text
+                  style={{
+                    fontSize: metrix.CustomFontSize(24 + 2),
+                    textAlign: "center",
+                    fontWeight: "bold",
+                    textDecorationLine: "underline",
+                    marginBottom: metrix.VerticalSize(10),
+                  }}
+                >
+                  {" "}
+                  {t("new_version_available")}
+                </Text>
+                <Text style={{ textAlign: "center" }}>
+                  {t("new_version_available_text")}
+                </Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  // alignItems: "center",
+                  width: metrix.HorizontalSize(270),
+                }}
+              >
+                <CustomButton
+                  onPress={() => {
+                    setshowUpdateAvailable(false);
+                  }}
+                  style={{
+                    marginTop: metrix.VerticalSize(25),
+                    width: metrix.HorizontalSize(70),
+                  }}
+                  textStyle={{ color: Colors.primary }}
+                  variant="outlined"
+                >
+                  {t("later")}
+                </CustomButton>
+                <CustomButton
+                  onPress={() => {
+                    handleRedirectToStore();
+                  }}
+                  style={{
+                    marginTop: metrix.VerticalSize(25),
+                    width: metrix.HorizontalSize(70),
+                  }}
+                  variant="filled"
+                >
+                  {t("update_now")}
+                </CustomButton>
+              </View>
+            </View>
+          </Modal>
           {isNetworkConnected ? (
             <MainNavigation />
           ) : (
